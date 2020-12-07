1ee879c5ba053e7deabae7c28ff5142cba9ddda4
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-07 21:25:35 +0900

    feat: Add string pushing feature that push string input according to input values

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,17 @@
 package com.company;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        if (input.length != 3) {
+            throw new IllegalArgumentException();
+        }
 
+        StringPusher stringPusher = new StringPusher(input[0], input[1], input[2]);
+        stringPusher.pushString();
+        System.out.println(stringPusher.getString());
     }
 }
--- src/com/company/StringPusher.java
+++ src/com/company/StringPusher.java
@@ -1,14 +1,12 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class StringPusher {
 	private static final int LOWER_BOUND = -100;
 	private static final int UPPER_BOUND = 100;
 
-	private final String string;
-	private final int numberOfPush;
-	private final String direction;
+	private String string;
+	private int numberOfPush;
+	private String direction;
 
 	public StringPusher(String string, String numberOfPush, String direction) {
 		validateInteger(numberOfPush);
@@ -39,21 +37,27 @@ public class StringPusher {
 		}
 	}
 
-	public boolean isLeft() {
+	private boolean isLeft() {
 		// positive + l || negative + r
 		return (0 <= numberOfPush && direction.equalsIgnoreCase("l")
 				|| numberOfPush < 0 && direction.equalsIgnoreCase("r"));
 	}
 
+	public void pushString() {
+		int cut = Math.abs(numberOfPush) % string.length();
+		if (cut == 0) {
+			return;
+		}
+		if (isLeft()) {
+			// push to left
+			string = string.substring(cut) + string.substring(0, cut);
+			return;
+		}
+		// push to right
+		string = string.substring(string.length() - cut) + string.substring(0, string.length() - cut);
+	}
+
 	public String getString() {
 		return string;
-	}
-
-	public int getNumberOfPush() {
-		return numberOfPush;
-	}
-
-	public String getDirection() {
-		return direction;
 	}
 }
