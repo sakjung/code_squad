bb9201b711dc63688d022e8ce27d7e6cd8c62142
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-07 22:38:57 +0900

    docs(README.md): add step2 features description

---
+++ src/com/company/Cube.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Cube {
+	/* CUBE
+	* R R W
+	* G C W
+	* G B B */
+
+	String[][] cube = {
+			{"R", "R", "W"},
+			{"G", "C", "W"},
+			{"G", "B", "B"}
+	};
+
+	public String[][] getCube() {
+		return cube;
+	}
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split(" ");
-        if (input.length != 3) {
-            throw new IllegalArgumentException();
-        }
+        String input = scanner.nextLine();
 
-        StringPusher stringPusher = new StringPusher(input[0], input[1], input[2]);
-        stringPusher.pushString();
-        System.out.println(stringPusher.getString());
     }
 }
