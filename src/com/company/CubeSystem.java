a1fb8bb04028b2457950b0c52e57be5f065d4502
sakjung <58401309+sakjung@users.noreply.github.com>
2020-12-07 23:28:38 +0900

    refactor: Modify getCommands method to return Stream

--- src/com/company/CubeSystem.java
+++ src/com/company/CubeSystem.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class CubeSystem {
 	private static final String CLI = "CUBE JOA> ";
@@ -9,8 +10,10 @@ public class CubeSystem {
 	public static void playCube(Cube cube, Scanner scanner) {
 	}
 
-	private static char[] getCommands(Scanner scanner) {
+	private static Stream<Character> getCommands(Scanner scanner) {
 		System.out.print(CLI);
-		return scanner.nextLine().toCharArray();
+		return scanner.nextLine()
+				.chars()
+				.mapToObj(command -> (char) command);
 	}
 }
